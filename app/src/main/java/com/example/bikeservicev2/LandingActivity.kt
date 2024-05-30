019b431baa046671e90f0fc66f21e04b632e6229
AmanCH3 <armanachaudhary53@gmail.com>
2024-05-30 17:21:15 +0545

    style : Change the apperance of the Login page

--- app/src/main/java/com/example/bikeservicev2/LandingActivity.kt
+++ app/src/main/java/com/example/bikeservicev2/LandingActivity.kt
@@ -2,8 +2,10 @@ package com.example.bikeservicev2
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.bikeservicev2.databinding.ActivityLandingpageBinding
 
 
@@ -11,10 +13,14 @@ class LandingActivity : AppCompatActivity() {
     lateinit var landingpageBinding: ActivityLandingpageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         landingpageBinding = ActivityLandingpageBinding.inflate(layoutInflater)
         setContentView(landingpageBinding.root)
+        val button = findViewById<Button>(R.id.loginButton)
+
+        button.setOnClickListener {
+            button.background = ContextCompat.getDrawable(this, R.drawable.button_background)
+        }
 
         landingpageBinding.loginButton.setOnClickListener{
             var intent = Intent(this, EmailActivity::class.java)
